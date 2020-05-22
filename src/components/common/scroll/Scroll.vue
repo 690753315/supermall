c61f018a11affb43fc77ced155f97edd9fe3d464
kuma <690753315@qq.com>
2020-05-22 17:06:01 +0800

    wan cheng 172

--- src/components/common/scroll/Scroll.vue
+++ src/components/common/scroll/Scroll.vue
@@ -16,8 +16,22 @@ export default {
       scroll: null
     }
   },
-  // 进行DOM操作要在mounted里进行，不能在created
+  props: {
+    // 控制BScroll的probeType属性
+    probeType:{
+      type: Number,
+      default: 0
+    },
+    // 控制BScroll的pullUpLoad属性
+    pullUpLoad:{
+      type: Boolean,
+      default: false
+    },
+  },
+  // 进行DOM操作、获取页面上的内容 要在mounted里进行，不能在created
   mounted(){
+
+    // 1. 创建BScroll对象
     this.scroll = new BScroll(this.$refs.wrapper, {
       // 默认情况下BScroll是不可以实时监听滚动位置的
       /*
@@ -26,7 +40,7 @@ export default {
         2在手指滚动的过程会监测，手机离开后的惯性滚动不监测
         3只要是滚动都会监测
       */
-      probeType: 3,
+      probeType: this.probeType,
       /*
         默认情况下BScroll禁止移动端的原生js点击事件
         设置 click: true, 才可以点击
@@ -36,17 +50,44 @@ export default {
       /*
         这个配置用于做上拉操作，默认为 false
       */
-      pullUpLoad: true,
+      pullUpLoad: this.pullUpLoad,
     })
 
-    this.scroll.scrollTo(0, 0, 300)
+    // 2. 监听滚动事件及滚动位置
+    this.scroll.on('scroll', position => {
+      // 输出滚动条的位置
+      // console.log(position)
 
+      // 滚动时， 发送自定义事件给父组件 并传递position
+      this.$emit('scroll', position)
+    })
 
+    // 3.上拉操作
+    this.scroll.on('pullingUp', () => {
+
+      this.$emit('pullingUp')
+      // 加个定时器，防止一直上拉加载。至少1秒才能进行一次上拉加载
+      // setTimeout(() => {
+      //   // 如果不加下面这一行，只会触发一次上拉操作
+      //   this.bscroll.finishPullUp()
+      // }, 1000)
+    });
 
   },
   methods:{
-    backTop(){
-      // this.scroll.scrollTo(0, 0, 300)
+    backTop(x, y, time){
+      // 滚动条回到指定位置
+      this.scroll.scrollTo(x, y, time)
+    },
+    finishPullUp(){
+      // 当上拉加载数据加载完毕后，需要调用此方法告诉 better-scroll 数据已加载。
+      this.scroll.finishPullUp()
+    },
+    refresh(){
+      /* 上拉加载完后，告诉BScroll重新计算content的高度，从而改变可滑动区域，
+        不然可能还是以原来的区域为可滑动高度
+      */
+      this.scroll.refresh()
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App'
 
 import router from './router'
 
+// 定义全局bus
+Vue.prototype.$bus = new Vue()
+
 new Vue({
   el: '#app',
   router,
